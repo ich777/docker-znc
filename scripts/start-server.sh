2b76672a8ebec90cd6a4a0a72530ab054d66768c
ich777 <christophhummer@gmail.com>
2022-08-16 09:55:35 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
+if [ ! -d ${DATA_DIR}/configs ]; then
+  mkdir -p ${DATA_DIR}/configs
+fi
+if [ ! -f ${DATA_DIR}/configs/znc.conf ]; then
+  echo "+-----------------------------------------------------"
+  echo "| No configuration found, copying default config file!"
+  echo "|"
+  echo "| The default credentials are:"
+  echo "| Username: admin"
+  echo "| Password: admin"
+  echo "+-----------------------------------------------------"
+  echo
+  cp /opt/znc.conf ${DATA_DIR}/configs/znc.conf
+fi
 
-echo "---Container under construction"
-sleep infinity
+if [ ! -f ${DATA_DIR}/znc.pem ]; then
+  /usr/bin/znc --datadir=${DATA_DIR} --makepem
+fi
 
 echo "---Prepare Server---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
@@ -9,4 +24,4 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${DATA_DIR}
-/usr/bin/znc
+/usr/bin/znc --datadir=${DATA_DIR} --foreground
